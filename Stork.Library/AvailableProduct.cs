62552130bcbfbae4b4f6230626a3d77552e9501b
Huseyin Caglayan <hcaglayan@hotmail.com>
2019-07-06 21:36:45 +0100

    available products added

---
+++ Stork.Library/AvailableProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stork.Library
+{
+    
+    public class AvailableProduct
+    {
+        public string ProductName { get; set; }
+        public string Version { get; set; }
+        public string DisplayVersion { get; set; }
+        public bool IsDefault { get; set; }
+        public string URL { get; set; }
+
+    }
+}
---
+++ Stork.Library/AvailableProductsCollection.cs
@@ -0,0 +1,42 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stork.Library
+{
+    public class AvailableProductsCollection : List<AvailableProduct>
+    {
+        private AvailableProductsCollection()
+        {
+
+
+        }
+
+
+        static public async Task<AvailableProductsCollection> FromUrlAsync(string url)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var result = await httpClient.GetAsync(url);
+                var text = await result.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+
+                using (var reader = new StringReader(text))
+                using (var csv = new CsvReader(reader))
+                {
+                    var records = csv.GetRecords<AvailableProduct>();
+
+                    AvailableProductsCollection availableProducts = new AvailableProductsCollection();
+                    availableProducts.AddRange(records);
+                    return availableProducts;
+                }
+
+            }
+        }
+    }
+}
+
